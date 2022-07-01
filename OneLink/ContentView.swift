4bcc5c42c5dcf6e0aee24b51673710a2b01d7d1a
Min-Su Kim <min-sukim@Min-Suui-MacBookPro-2.local>
2022-07-02 00:31:59 +0900

    🎉 원링크 프로젝트 생성

---
+++ OneLink/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  OneLink
+//
+//  Created by Min-Su Kim on 2022/07/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+}
---
+++ OneLink/OneLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneLinkApp.swift
+//  OneLink
+//
+//  Created by Min-Su Kim on 2022/07/02.
+//
+
+import SwiftUI
+
+@main
+struct OneLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
